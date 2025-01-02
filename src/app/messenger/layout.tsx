5e640a8fd6c859d4a7997ab847b2bd76648161f4
Khuyag Buyan-Urt <buynaa.khuyag@gmail.com>
2025-01-02 17:51:43 +0800

    profile slide

--- src/app/messenger/layout.tsx
+++ src/app/messenger/layout.tsx
@@ -1,58 +1,202 @@
 "use client";
 
-import * as React from "react";
-import { Search } from "lucide-react";
+import { ReactNode, useEffect, useRef, useState } from "react";
+
+import { ChevronRight, ChevronLeft } from "lucide-react";
 
 import { Separator } from "../../components/ui/separator";
-import { Input } from "../../components/ui/input";
-import {
-  Tabs,
-  TabsContent,
-  TabsList,
-  TabsTrigger,
-} from "../../components/ui/tabs";
 import { MailList } from "../../components/message-list";
 import { type Mail } from "@/app/messenger/data";
-import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import { useSelector } from "react-redux";
+import { getRequest } from "@/utils/handlers";
+import { UserType } from "@/types/userType";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { SIDE_BAR_WIDTH } from "@/utils/consts";
+import { Button } from "@/components/ui/button";
 
 interface MailProps {
   accounts: {
     label: string;
     email: string;
-    icon: React.ReactNode;
+    icon: ReactNode;
   }[];
   mails: Mail[];
   navCollapsedSize: number;
 }
 
+const PROFILE_WIDTH = 90;
+const MARGIN_LEFT = 5;
+const MARGIN_RIGHT = 5;
+const SCROLL_TIMEOUT = 200;
+const ITEM_WIDTH = PROFILE_WIDTH + MARGIN_LEFT + MARGIN_RIGHT;
+
+const calcScrollPosition = (scrollLeft: number) => {
+  return Math.round(scrollLeft / ITEM_WIDTH);
+};
+
+// Хамги
 const InboxTab = ({ accounts, mails, navCollapsedSize }: MailProps) => {
+  const userKey = useSelector((state) => state.user.userKey);
+  const [friends, setFriends] = useState<UserType[]>([]);
+  // const [isScrolling, setIsScrolling] = useState(false);
+  const scrollTimeout = useRef<NodeJS.Timeout | null>(null); // Use useRef to store the timeout
+
+  const ref = useRef<HTMLDivElement>(null);
+
+  const handleScroll = () => {
+    // setIsScrolling(true);
+    if (scrollTimeout.current) {
+      clearTimeout(scrollTimeout.current);
+    }
+
+    scrollTimeout.current = setTimeout(() => {
+      // setIsScrolling(false);
+      if (ref.current) {
+        if (
+          ref.current.scrollWidth - ref.current.scrollLeft <
+          ref.current.clientWidth
+        )
+          return;
+        ref.current.scrollTo({
+          left: calcScrollPosition(ref.current.scrollLeft) * ITEM_WIDTH,
+          behavior: "smooth",
+        });
+      }
+    }, SCROLL_TIMEOUT);
+  };
+
+  useEffect(() => {
+    return () => {
+      if (scrollTimeout.current) {
+        clearTimeout(scrollTimeout.current); // Clear the timeout on cleanup
+      }
+    };
+  }, []);
+  useEffect(() => {
+    if (!userKey) return;
+
+    const fetchUsers = async () => {
+      try {
+        const users = await getRequest<{ users: UserType[] }>("/auth/users", {
+          userKey,
+        });
+        setFriends(
+          [
+            ...(users?.data?.users || []),
+            ...(users?.data?.users || []),
+            ...(users?.data?.users || []),
+            ...(users?.data?.users || []),
+            ...(users?.data?.users || []),
+            ...(users?.data?.users || []),
+            ...(users?.data?.users || []),
+            ...(users?.data?.users || []),
+          ] || []
+        );
+      } catch (error) {
+        console.error("Failed to fetch users:", error);
+      }
+    };
+    fetchUsers();
+  }, [userKey]);
+
+  const calculatedWidth = `calc(50vw - ${SIDE_BAR_WIDTH}px)`;
   return (
-    <Tabs defaultValue="all" className="h-full">
-      <div className="flex items-center px-2 py-4">
-        <h1 className="text-xl font-bold">Inbox</h1>
-        <TabsList className="ml-auto">
-          <TabsTrigger value="all" className="text-zinc-600 dark:text-zinc-200">
-            All mail
-          </TabsTrigger>
-          <TabsTrigger
-            value="unread"
-            className="text-zinc-600 dark:text-zinc-200"
-          >
-            Unread
-          </TabsTrigger>
-        </TabsList>
+    <div className="w-full overflow-x-hidden">
+      <h1 className="text-xl font-bold m-4">Inbox</h1>
+      <Separator />
+      <div className="flex relative">
+        <Button
+          variant={"ghost"}
+          size={"lg"}
+          // disabled={ref.current === null || ref.current.scrollLeft <= 0}
+          className="absolute left-0 top-1/2 transform -translate-y-1/2 p-2 z-10 "
+          onClick={() => {
+            if (ref.current && ref.current.scrollLeft > 0) {
+              ref.current.scrollTo({
+                left:
+                  calcScrollPosition(ref.current.scrollLeft - ITEM_WIDTH) *
+                  ITEM_WIDTH,
+                behavior: "smooth",
+              });
+            }
+          }}
+        >
+          <ChevronLeft />
+        </Button>
+
+        <div
+          className="flex items-center overflow-x-scroll hide-scrollbar"
+          style={{ maxWidth: calculatedWidth }}
+          ref={ref}
+          onScroll={handleScroll}
+        >
+          {friends.map((friend, index) => (
+            <figure
+              key={index}
+              style={{
+                width: PROFILE_WIDTH,
+                marginLeft: MARGIN_LEFT,
+                marginRight: MARGIN_RIGHT,
+                height: PROFILE_WIDTH,
+                marginTop: 5,
+                marginBottom: 5,
+                borderRadius: "50%",
+              }}
+              className="shrink-0"
+            >
+              <Avatar
+                key={friend.id}
+                style={{
+                  width: "100%",
+                  height: "100%",
+                }}
+              >
+                {/* <AvatarImage
+                  src="https://github.com/shadcn.png"
+                  alt="@shadcn"
+                  style={{
+                    width: "100%",
+                    height: "100%",
+                    objectFit: "cover", // Maintains the aspect ratio and fills the container
+                  }}
+                /> */}
+                <AvatarFallback>{index}</AvatarFallback>
+              </Avatar>
+            </figure>
+          ))}
+        </div>
+
+        <Button
+          variant={"ghost"}
+          size={"lg"}
+          className="absolute right-0 top-1/2 transform -translate-y-1/2 p-2 z-10"
+          disabled={
+            ref.current &&
+            ref.current.scrollWidth - ref.current.scrollLeft <
+              ref.current.clientWidth
+          }
+          onClick={() => {
+            if (ref.current) {
+              if (
+                ref.current.scrollWidth - ref.current.scrollLeft <
+                ref.current.clientWidth
+              )
+                return;
+              ref.current.scrollTo({
+                left:
+                  calcScrollPosition(ref.current.scrollLeft + ITEM_WIDTH) *
+                  ITEM_WIDTH,
+                behavior: "smooth",
+              });
+            }
+          }}
+        >
+          <ChevronRight size={40} />
+        </Button>
       </div>
       <Separator />
-      <ScrollArea>
-        <ScrollBar orientation="horizontal" />
-      </ScrollArea>
-      <TabsContent value="all" className="m-0">
-        <MailList items={[]} />
-      </TabsContent>
-      <TabsContent value="unread" className="m-0">
-        <MailList items={[]} />
-      </TabsContent>
-    </Tabs>
+      <MailList items={[]}></MailList>
+    </div>
   );
 };
 
